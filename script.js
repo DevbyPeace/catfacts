05a48d182034de0b37e253a6f3050f994bd77997
Peace <devbypeace@gmail.com>
2025-05-02 17:46:42 +0100

    switched from XMLHTTP to fetch

--- script.js
+++ script.js
@@ -11,20 +11,29 @@
 const button = document.querySelector(".change-btn");
 const factDisp = document.querySelector(".random-fact");
 
-function catapi() {
-  console.log("linked");
-  const request = new XMLHttpRequest();
-  request.open("GET", "https://catfact.ninja/fact");
-  request.send();
+// function catapi() {
+//   // console.log("linked");
+//   const request = new XMLHttpRequest();
+//   request.open("GET", "https://catfact.ninja/fact");
+//   request.send();
 
-  request.addEventListener("load", () => {
-    console.log("loaded");
-    const factData = JSON.parse(request.responseText);
-    console.log(factData);
-    console.log(factData.fact);
-    factDisp.textContent = factData.fact;
-  });
-}
-catapi();
+//   request.addEventListener("load", () => {
+//     // console.log("loaded");
+//     const factData = JSON.parse(request.responseText);
+//     console.log(factData);
+//     console.log(factData.fact);
+//     factDisp.textContent = factData.fact;
+//   });
+// }
+// catapi();
 
-button.addEventListener('click', catapi);
+// button.addEventListener("click", catapi);
+
+const factGetter = function () {
+  fetch("https://catfact.ninja/fact")
+    .then((response) => response.json())
+    .then((data) => (factDisp.textContent = data.fact));
+};
+
+factGetter();
+button.addEventListener("click", factGetter);
